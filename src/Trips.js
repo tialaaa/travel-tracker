302f70b5d7bfdb3da11e8b565505492638a9fd21
Tiala Young <tialayoung@gmail.com>
2023-04-20 16:54:21 -0600

    Finish findTripsBy method and testing

--- src/Trips.js
+++ src/Trips.js
@@ -3,43 +3,48 @@ class Trips {
     this.allTrips = arrayOfTrips
   };
 
-  // findTripsBy(searchKey, searchValue) {
-  //   // variation for only 1 search type at a time
-  //   return this.allTrips.filter(trip => trip[searchKey] === searchValue)
+  findTripsBy(searchKey, searchValue) {
+    // If this isn't used to filter by status, change to userID only
+    return this.allTrips.filter(trip => trip[searchKey] === searchValue)
+  };
+
+  // Super dynamic function below - contains errors
+  // findTripsBy(IdNumber, ...status) {
+  //   let currentTripsFilter;
+  //   console.log(status)
+
+  //   // if (!IdNumber) {
+  //   //   return console.log('Error: userID needs to be a number')
+  //   // }
+
+  //   if (IdNumber || IdNumber === undefined && status.length === 0) {
+  //     // console.log('row 15')
+  //     return currentTripsFilter = this.allTrips.filter(trip => trip.userID === IdNumber);
+  //   } else {
+  //     console.log('row 18')
+  //     currentTripsFilter = this.allTrips;
+  //   };
+
+  //   if (status.length === 1) {
+  //     console.log('row 23')
+  //     return currentTripsFilter.filter((trip => trip.status === status[0]))
+  //   } else {
+  //     console.log('row 26')
+  //     return currentTripsFilter.reduce((acc, currentTrip) => {
+  //       status.forEach(status => {
+  //         if (currentTrip.status === status) {
+  //           acc.push(currentTrip)
+  //         }
+  //       })
+
+  //       return acc
+  //     }, [])
+  //   };
   // };
 
-  findTripsBy(IdNumber, ...status) {
-    let currentTripsFilter;
-    console.log(status)
-
-    // if (!IdNumber) {
-    //   return console.log('Error: userID needs to be a number')
-    // }
-
-    if (IdNumber || IdNumber === undefined && status.length === 0) {
-      // console.log('row 15')
-      return currentTripsFilter = this.allTrips.filter(trip => trip.userID === IdNumber);
-    } else {
-      console.log('row 18')
-      currentTripsFilter = this.allTrips;
-    };
-
-    if (status.length === 1) {
-      console.log('row 23')
-      return currentTripsFilter.filter((trip => trip.status === status[0]))
-    } else {
-      console.log('row 26')
-      return currentTripsFilter.reduce((acc, currentTrip) => {
-        status.forEach(status => {
-          if (currentTrip.status === status) {
-            acc.push(currentTrip)
-          }
-        })
-
-        return acc
-      }, [])
-    };
-  };
+  calculateTotalCost(userID) {
+    // need Destination class to finish
+  }
 }
 
 export default Trips;
