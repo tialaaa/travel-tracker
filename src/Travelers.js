d35a32b651d1d9a7f43f24d54563d5da6fe12bb6
Tiala Young <tialayoung@gmail.com>
2023-04-20 12:33:41 -0600

    Add Travelers constructor and methods

--- src/Travelers.js
+++ src/Travelers.js
@@ -1,9 +1,22 @@
 class Travelers {
   constructor(arrayOfTravelers) {
     this.allTravelers = arrayOfTravelers
-  }
+    this.currentUser = undefined
+  };
 
-  
+  findUserById(searchId) {
+    this.currentUser = this.allTravelers.find(traveler => traveler.id === searchId);
+
+    return this.currentUser;
+  };
+
+  printUserInfo(key) {
+    if (!this.currentUser || !this.currentUser[key]) {
+      return undefined
+    };
+
+    return this.currentUser[key];
+  };
 }
 
 export default Travelers;
