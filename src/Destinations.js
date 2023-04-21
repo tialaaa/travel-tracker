2af09201aedca134ef1623308e2c7233a95219da
Tiala Young <tialayoung@gmail.com>
2023-04-20 20:25:48 -0600

    Finish Destinations class and methods

--- src/Destinations.js
+++ src/Destinations.js
@@ -3,7 +3,20 @@ class Destinations {
     this.allDestinations = arrayOfDestinations
   };
 
+  findById(destinationID) {
+    return this.allDestinations.find(place => place.id === destinationID);
+  };
 
-}
+  calculateTripCost(destinationID, numDays, numTravelers) {
+    let currentDestination = this.allDestinations.find(place => place.id === destinationID);
+    
+    if (!currentDestination) {
+      return 0;
+    };
+
+    return Math.round((numDays * currentDestination.estimatedLodgingCostPerDay
+      + numTravelers * currentDestination.estimatedFlightCostPerPerson) * 1.1);
+  };
+};
 
 export default Destinations;
