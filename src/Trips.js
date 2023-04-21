2009158ea674964fe70143a6a878810ea3362a74
Tiala Young <tialayoung@gmail.com>
2023-04-20 20:25:26 -0600

    Remove commented out partial method

--- src/Trips.js
+++ src/Trips.js
@@ -8,40 +8,6 @@ class Trips {
     return this.allTrips.filter(trip => trip[searchKey] === searchValue)
   };
 
-  // Super dynamic function below - contains errors
-  // findTripsBy(IdNumber, ...status) {
-  //   let currentTripsFilter;
-  //   console.log(status)
-
-  //   // if (!IdNumber) {
-  //   //   return console.log('Error: userID needs to be a number')
-  //   // }
-
-  //   if (IdNumber || IdNumber === undefined && status.length === 0) {
-  //     // console.log('row 15')
-  //     return currentTripsFilter = this.allTrips.filter(trip => trip.userID === IdNumber);
-  //   } else {
-  //     console.log('row 18')
-  //     currentTripsFilter = this.allTrips;
-  //   };
-
-  //   if (status.length === 1) {
-  //     console.log('row 23')
-  //     return currentTripsFilter.filter((trip => trip.status === status[0]))
-  //   } else {
-  //     console.log('row 26')
-  //     return currentTripsFilter.reduce((acc, currentTrip) => {
-  //       status.forEach(status => {
-  //         if (currentTrip.status === status) {
-  //           acc.push(currentTrip)
-  //         }
-  //       })
-
-  //       return acc
-  //     }, [])
-  //   };
-  // };
-
   calculateTotalCost(userID, destinationsArray) {
     let filteredTrips;
 
@@ -57,12 +23,13 @@ class Trips {
       let locationInfo = destinationsArray.allDestinations.find(destination => destination.id === current.destinationID);
 
       if (locationInfo) {
-        acc += Math.round((current.duration * locationInfo.estimatedLodgingCostPerDay + current.travelers * locationInfo.estimatedFlightCostPerPerson) * 1.1);
+        acc += Math.round((current.duration * locationInfo.estimatedLodgingCostPerDay
+          + current.travelers * locationInfo.estimatedFlightCostPerPerson) * 1.1);
       }
 
       return acc;
     }, 0);
   };
-}
+};
 
 export default Trips;
