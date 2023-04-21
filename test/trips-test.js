4634551c121098c69800436ed97a0c6250055e43
Tiala Young <tialayoung@gmail.com>
2023-04-21 15:36:12 -0600

    Add sorting to findSortedTripsBy method and tests

--- test/trips-test.js
+++ test/trips-test.js
@@ -75,7 +75,8 @@ describe('Trips class tests', () => {
         "userID": 37,
         "destinationID": 17,
         "travelers": 5,
-        "date": "2022/5/28",
+        // added 0 to month
+        "date": "2022/05/28",
         "duration": 20,
         "status": "approved",
         "suggestedActivities": []
@@ -174,8 +175,8 @@ describe('Trips class tests', () => {
     expect(tripsRepo.allData).to.deep.equal(dataArray)
   });
 
-  it('should find all trips for a given userID number', () => {
-    expect(tripsRepo.findTripsBy('userID', 44)).to.deep.equal([
+  it('should find all trips for a given userID number and return them sorted newest to oldest', () => {
+    expect(tripsRepo.findSortedTripsBy('userID', 44)).to.deep.equal([
       {
         "id": 1,
         "userID": 44,
@@ -183,16 +184,6 @@ describe('Trips class tests', () => {
         "travelers": 1,
         "date": "2022/09/16",
         "duration": 8,
-        "status": "approved",
-        "suggestedActivities": []
-      },
-      {
-        "id": 46,
-        "userID": 44,
-        "destinationID": 33,
-        "travelers": 2,
-        "date": "2020/08/24",
-        "duration": 11,
         "status": "approved",
         "suggestedActivities": []
       },
@@ -205,22 +196,22 @@ describe('Trips class tests', () => {
         "duration": 8,
         "status": "pending",
         "suggestedActivities": []
+      },
+      {
+        "id": 46,
+        "userID": 44,
+        "destinationID": 33,
+        "travelers": 2,
+        "date": "2020/08/24",
+        "duration": 11,
+        "status": "approved",
+        "suggestedActivities": []
       }
     ])
   });
 
-  it('should find all trips for a given status string', () => {
-    expect(tripsRepo.findTripsBy('status', 'approved')).to.deep.equal([
-      {
-        "id": 1,
-        "userID": 44,
-        "destinationID": 49,
-        "travelers": 1,
-        "date": "2022/09/16",
-        "duration": 8,
-        "status": "approved",
-        "suggestedActivities": []
-      },
+  it('should find all trips for a given status string and return them sorted newest to oldest', () => {
+    expect(tripsRepo.findSortedTripsBy('status', 'approved')).to.deep.equal([
       {
         "id": 2,
         "userID": 35,
@@ -232,32 +223,12 @@ describe('Trips class tests', () => {
         "suggestedActivities": []
       },
       {
-        "id": 3,
-        "userID": 3,
-        "destinationID": 22,
-        "travelers": 4,
-        "date": "2022/05/22",
-        "duration": 17,
-        "status": "approved",
-        "suggestedActivities": []
-      },
-      {
-        "id": 4,
-        "userID": 43,
-        "destinationID": 14,
-        "travelers": 2,
-        "date": "2022/02/25",
-        "duration": 10,
-        "status": "approved",
-        "suggestedActivities": []
-      },
-      {
-        "id": 5,
-        "userID": 42,
-        "destinationID": 29,
-        "travelers": 3,
-        "date": "2022/04/30",
-        "duration": 18,
+        "id": 1,
+        "userID": 44,
+        "destinationID": 49,
+        "travelers": 1,
+        "date": "2022/09/16",
+        "duration": 8,
         "status": "approved",
         "suggestedActivities": []
       },
@@ -276,8 +247,38 @@ describe('Trips class tests', () => {
         "userID": 37,
         "destinationID": 17,
         "travelers": 5,
-        "date": "2022/5/28",
+        "date": "2022/05/28",
         "duration": 20,
+        "status": "approved",
+        "suggestedActivities": []
+      },
+      {
+        "id": 3,
+        "userID": 3,
+        "destinationID": 22,
+        "travelers": 4,
+        "date": "2022/05/22",
+        "duration": 17,
+        "status": "approved",
+        "suggestedActivities": []
+      },
+      {
+        "id": 5,
+        "userID": 42,
+        "destinationID": 29,
+        "travelers": 3,
+        "date": "2022/04/30",
+        "duration": 18,
+        "status": "approved",
+        "suggestedActivities": []
+      },
+      {
+        "id": 4,
+        "userID": 43,
+        "destinationID": 14,
+        "travelers": 2,
+        "date": "2022/02/25",
+        "duration": 10,
         "status": "approved",
         "suggestedActivities": []
       },
@@ -294,18 +295,8 @@ describe('Trips class tests', () => {
     ])
   });
 
-  it('should find all trips for a different status string', () => {
-    expect(tripsRepo.findTripsBy('status', 'pending')).to.deep.equal([
-      {
-        "id": 47,
-        "userID": 28,
-        "destinationID": 32,
-        "travelers": 3,
-        "date": "2019/12/10",
-        "duration": 14,
-        "status": "pending",
-        "suggestedActivities": []
-      },
+  it('should find all trips for a different status string and return them sorted newest to oldest', () => {
+    expect(tripsRepo.findSortedTripsBy('status', 'pending')).to.deep.equal([
       {
         "id": 48,
         "userID": 44,
@@ -315,20 +306,30 @@ describe('Trips class tests', () => {
         "duration": 8,
         "status": "pending",
         "suggestedActivities": []
+      },
+      {
+        "id": 47,
+        "userID": 28,
+        "destinationID": 32,
+        "travelers": 3,
+        "date": "2019/12/10",
+        "duration": 14,
+        "status": "pending",
+        "suggestedActivities": []
       }
     ])
   });
 
   it('should return an empty array if the first argument passed is not an exact match', () => {
-    expect(tripsRepo.findTripsBy('userId', 44)).to.be.empty
+    expect(tripsRepo.findSortedTripsBy('userId', 44)).to.be.empty
   });
 
   it('should return an empty array if the second argument passed is not an exact match', () => {
-    expect(tripsRepo.findTripsBy('userID', '44')).to.be.empty
+    expect(tripsRepo.findSortedTripsBy('userID', '44')).to.be.empty
   });
 
   it('should return an empty array if the second argument passed is not included in the data', () => {
-    expect(tripsRepo.findTripsBy('userID', 1)).to.be.empty
+    expect(tripsRepo.findSortedTripsBy('userID', 1)).to.be.empty
   });
 
   it('should calculate the total spent on past trips by a specific user', () => {
